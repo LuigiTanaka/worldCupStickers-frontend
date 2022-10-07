3fb00e807340cd18448e22b00b324c9f1029d730
luigi tanaka <luigi.tanaka12@gmail.com>
2022-10-07 15:16:15 -0300

    feat: adiciona quadro geral

---
+++ src/components/GeneralFramework.js
@@ -0,0 +1,110 @@
+import { useContext } from "react";
+import UserContext from "../contexts/UserContext";
+import styled from "styled-components";
+import album from "../assets/images/album.jpg"
+
+export default function GeneralFramework() {
+    const { user } = useContext(UserContext);
+
+    const total = 678;
+    const missing = 610;
+    const repeated = 20;
+
+    return (
+        <Container>
+            <LeftSide>
+                <h2>{`${user.userData.username.toUpperCase()}'S ALBUM`}</h2>
+                <img src={album} alt="album"/>
+            </LeftSide>
+            <RightSide>
+                <Porcentage>
+                    <h3>{`10% (68/678)`}</h3>
+                </Porcentage>
+                <Data>
+                    <div>
+                        <h4><strong>Total in the album: </strong>{total}</h4>
+                        <h4><strong>Total you have: </strong>{total-missing}</h4>
+                    </div>
+                    <div>
+                        <h4><strong>Total missing: </strong>{missing}</h4>
+                        <h4><strong>Total repeated: </strong>{repeated}</h4>
+                    </div>
+                </Data>
+            </RightSide>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    width: 860px;
+    height: 200px;
+    padding: 20px;
+    display: flex;
+    background-color: #F0F0E4;
+    border-radius: 8px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const LeftSide = styled.div`
+    width: 200px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-right: 20px;
+    border-right: 2px solid #000000;
+
+    h2 {
+        font-size: 24px;
+        font-weight: 700;
+        margin-bottom: 10px;
+        color: #6b0b1a;
+    }
+
+    img {
+        width: 100px;
+        height: 120px;
+    }
+`
+
+const RightSide = styled.div`
+    width: 700px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 4px 20px;
+`
+
+const Porcentage = styled.div`
+    width: 100%;
+
+    h3 {
+        font-size: 24px;
+        font-weight: 700;
+    }
+`
+
+const Data = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    gap: 80px;
+
+    div {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    h4 {
+        font-size: 24px;
+        font-weight: 300;
+    }
+
+    strong {
+        font-weight: 800;
+    }
+`
--- src/pages/StickersPage.js
+++ src/pages/StickersPage.js
@@ -1,28 +1,12 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import Header from "../components/Header";
 import styled from "styled-components";
 import { Triangle } from 'react-loader-spinner';
-import UserContext from "../contexts/UserContext";
-import album from "../assets/images/album.jpg"
+import GeneralFramework from "../components/GeneralFramework";
 
 export default function StickersPage() {
-    const { user } = useContext(UserContext);
-
     const [loading, setLoading] = useState("");
-
     //<Triangle color="#FFFFFF" height="160" width="160" />
-
-    function showGeneralFramework() {
-        return (
-            <GeneralFramework>
-                <LeftSide>
-                    <h2>{`${user.userData.username.toUpperCase()}'S ALBUM`}</h2>
-                    <img src={album} alt="album"/>
-                </LeftSide>
-            </GeneralFramework>
-        );
-    }
-
 
     function showStickers() {
         return (
@@ -31,12 +15,11 @@ export default function StickersPage() {
     }
 
     const stickers = showStickers();
-    const generalFramework = showGeneralFramework();
 
     function showMainBody() {
         return (
             <MainBody>
-                {generalFramework}
+                <GeneralFramework />
                 {stickers}
             </MainBody>
         );
@@ -77,33 +60,5 @@ const MainBody = styled.div`
     justify-content: center;
     gap: 30px;
     position: relative;
-    padding-top: 160px;
-`
-
-const GeneralFramework = styled.div`
-    width: 900px;
-    height: 200px;
-    padding: 20px;
-    display: flex;
-    background-color: #F0F0E4;
-`
-
-const LeftSide = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    h2 {
-        font-size: 24px;
-        font-weight: 700;
-        margin-bottom: 10px;
-        color: #6b0b1a;
-    }
-
-    img {
-        width: 100px;
-        height: 120px;
-    }
+    padding-top: 140px;
 `
