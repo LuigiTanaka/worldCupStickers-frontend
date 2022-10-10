b37551b44a53d89edfb549ec949c2ef9334dea69
luigi tanaka <luigi.tanaka12@gmail.com>
2022-10-10 13:55:31 -0300

    style: ajusta tamanho do titulo

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -115,13 +115,12 @@ const Title = styled.div`
     background-color: #601717;
     display: flex;
     flex-direction: column;
-    padding: 100px 20px;
-    align-items: center;
+    padding: 100px 30px;
     color: #FFFFFF;
 
     h1 {
         font-weight: 800;
-        font-size: 86px;
+        font-size: 80px;
         line-height: 117px;
         letter-spacing: 0.05em;
     }
